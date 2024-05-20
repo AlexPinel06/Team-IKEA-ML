07e33370d3f9ea50b51d9cec07ef76697861993c
AlexP <145569112+AlexPinel06@users.noreply.github.com>
2024-05-20 17:30:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,34 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import torch
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import CamembertTokenizer, CamembertForSequenceClassification
 from sklearn.preprocessing import LabelEncoder
 import re
 import os
+import gdown
+
+# Fonction pour télécharger le modèle depuis Google Drive
+def download_file_from_google_drive(file_id, dest):
+    gdown.download(f'https://drive.google.com/uc?export=download&id={file_id}', dest, quiet=False)
+
+# IDs des fichiers sur Google Drive
+model_file_id = 'YOUR_MODEL_FILE_ID'
+classes_file_id = 'YOUR_CLASSES_FILE_ID'
+
+# Chemins vers les fichiers de modèle et les classes
+model_path = 'model/trained_model.pt'
+classes_path = 'model/classes.npy'
+
+# Créer le répertoire du modèle s'il n'existe pas
+if not os.path.exists('model'):
+    os.makedirs('model')
+
+# Télécharger les fichiers si nécessaire
+if not os.path.exists(model_path):
+    download_file_from_google_drive(model_file_id, model_path)
+
+if not os.path.exists(classes_path):
+    download_file_from_google_drive(classes_file_id, classes_path)
 
 # Fonction pour encoder les textes
 def encode_text(data, tokenizer):
@@ -15,23 +39,10 @@ def encode_text(data, tokenizer):
     encodings['punctuation_count'] = torch.tensor(data['punctuation_count'].tolist())
     return encodings
 
-# Chemins vers les fichiers de modèle et les classes
-model_path = 'model/trained_model.pt'
-classes_path = 'model/classes.npy'
-
-# Vérification de l'existence des fichiers nécessaires
-if not os.path.exists(model_path):
-    st.error(f"Le fichier de modèle {model_path} n'existe pas.")
-    st.stop()
-
-if not os.path.exists(classes_path):
-    st.error(f"Le fichier des classes {classes_path} n'existe pas.")
-    st.stop()
-
 # Chargement du tokenizer et du modèle
 try:
-    tokenizer = AutoTokenizer.from_pretrained('distilbert-base-uncased')
-    model = AutoModelForSequenceClassification.from_pretrained('distilbert-base-uncased', num_labels=3)
+    tokenizer_camembert = CamembertTokenizer.from_pretrained('camembert-base')
+    model = CamembertForSequenceClassification.from_pretrained('camembert-base', num_labels=3)
     
     # Vérifier si le fichier du modèle est valide
     model_state_dict = torch.load(model_path, map_location=torch.device('cpu'))
@@ -62,7 +73,7 @@ if st.button('Prédire la Difficulté'):
         df = pd.DataFrame({'sentence': [sentence]})
         df['text_length'] = df['sentence'].apply(len)
         df['punctuation_count'] = df['sentence'].apply(lambda x: len(re.findall(r'[^\w\s]', x)))
-        encodings = encode_text(df, tokenizer)
+        encodings = encode_text(df, tokenizer_camembert)
 
         # Faire la prédiction
         try:
